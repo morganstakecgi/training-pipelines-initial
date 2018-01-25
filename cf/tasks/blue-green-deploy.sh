61615c4c9340ab901c9fa2739a6a747d720c0592
morgan stake <morgan.stake@cgi.com>
2018-01-25 15:36:34 -0700

    test

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -38,9 +38,11 @@ echo "routes_url: shows the routes in the space: ${routes_url}"
 
 # get the apps on on the route in the domain
 apps_url=`cf curl $routes_url | jq -r '.resources[].entity | select(.host=="'"$CF_HOSTNAME"'") | select(.domain_guid=="'"$domain_guid"'") | .apps_url'`
+set -x
 
 # Fetch the app names assigned to the hostname
 app_names=`(cf curl $apps_url | jq -r '.resources[].entity.name')`
+
 
 for name in $app_names; do
     if [ "$name" != "$app_name" ]
